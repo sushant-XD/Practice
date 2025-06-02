c914139f56b8a9b6fd68ea0f0c2fe30454cbd4de
sushant-XD <sushantdhakal07@gmail.com>
2025-06-01 20:38:47 -0500

    added quick sort

--- sorting_algorithms/Firstprogram.cpp
+++ sorting_algorithms/Firstprogram.cpp
@@ -3,19 +3,20 @@
 // bubble sort
 // compare with previous value to sort (similar to how bubbles rise from the bottom) 
 void printVector(std::vector<int>&  original);
+void mergesort(std::vector<int> &arr, int left, int right);
+
 std::vector<int> SelectionSort(std::vector<int>& original);
 std::vector<int> BubbleSort(std::vector<int>& original);
 std::vector<int> InsertionSort(std::vector<int> &original);
 std::vector<int> MergeSort(std::vector<int> &original);
-
-void mergesort(std::vector<int> &arr, int left, int right);
+std::vector<int> QuickSort(std::vector<int> &original);
 
 int main (int argc, char *argv[]) {
-  std::vector<int> original = {2,1,4,3,1,22,199,12,21,1,2,3321};
+  std::vector<int> original = {2,1,4,3,1,22,199,12,21};
   std::vector<int> sortedArray;
   std::cout << "Enter the type of Sorting algorithms to use\n";
   std::cout << "---------------------------------------------\n";
-  std::cout << "  1 -> BubbleSort \n  2 -> SelectionSort \n  3 -> InsertionSort \n 4 -> MergeSort";
+  std::cout << "  1 -> BubbleSort \n  2 -> SelectionSort \n  3 -> InsertionSort \n 4 -> MergeSort \n 5 -> QuickSort \n";
   int input = 0;
   std::cin >> input;
 
@@ -33,6 +34,8 @@ int main (int argc, char *argv[]) {
       sortedArray = InsertionSort(original);
     case 4:
       sortedArray = MergeSort(original);
+    case 5:
+      sortedArray = QuickSort(original);
     default:
       std::cout << "Select valid options";
       break;
@@ -86,7 +89,7 @@ std::vector<int> BubbleSort(std::vector<int>& original){
 // keep adding elements and check everytime if we need to swap with the leftmost number until we don't have to 
 // sounds pretty easy, lets go
 std::vector<int> InsertionSort(std::vector<int> &original){
-  std::cout << "Insertion Sort";
+  std::cout << "Insertion Sort" << std::endl;
   std::vector<int> sorted_array = {original[0]};
   for(size_t i=1;i<original.size();i++){
     sorted_array.push_back(original[i]);
@@ -99,6 +102,49 @@ std::vector<int> InsertionSort(std::vector<int> &original){
     }
   }
   return sorted_array;
+}
+
+int partition(std::vector<int> &original, int left, int right){
+  int pivot = original[right];
+  int i=left, j= right-1; 
+  while(i < right && j >= i){
+    while(original[i] < pivot){
+      i++;
+    }
+    while(original[j] > pivot){
+      j--;
+    }
+    if(i<=j){
+      std::swap(original[i],original[j]);
+      i++;
+      j--;
+    }
+  }
+  std::swap(original[i], original[right]);
+  return i;
+}
+
+void quick_sort(std::vector<int> &original, int left, int right){
+  if(left >= right || right < 0 || left < 0){
+    return;
+  }
+  int index = partition(original, left, right);
+  quick_sort(original, left, index -1);
+  quick_sort(original, index+1, right);
+}
+
+
+
+// sounds simple but is not
+// take a value (last value for now), and consider it a node
+// start pointers left and right
+std::vector<int> QuickSort(std::vector<int> &original){
+  std::cout << "Quick Sort" << std::endl;
+  if(original.size() <= 1){
+    return original;
+  }
+  quick_sort(original, 0, original.size() - 1);
+  return original;
 }
 
 void merge(std::vector<int> &arr, int left, int mid, int right){
---
+++ sorting_algorithms/build/CMakeFiles/FirstProject.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FirstProject.dir/Firstprogram.cpp.o"
+  "CMakeFiles/FirstProject.dir/Firstprogram.cpp.o.d"
+  "FirstProject"
+  "FirstProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FirstProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
